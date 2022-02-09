223aac90565912c7237cec4cd67ed4605349072d
izabelle avena <izabelleavena@hotmail.com>
2022-02-09 09:49:01 -0300

    primeiro commit

---
+++ function.js
@@ -0,0 +1,21 @@
+
+
+/* function montaListagemExibicao (){
+    var listagemExibir = [];
+    let totalPaginas = Math.ceil(this.listagem.length / parseInt(this.porPagina));
+    let itemInicial = (parseInt(this.paginaAtual) * parseInt(this.porPagina)) - parseInt(this.porPagina);
+    let itemFinal = itemInicial + parseInt(this.porPagina);
+    if(this.paginaAtual === totalPaginas){
+        itemFinal = this.listagem.length;
+    }
+    if(this.paginaAtual <= totalPaginas){
+        for(let i = itemInicial; i < itemFinal; i++){
+            this.listagemExibir.push(this.listagem[i]);
+            itemInicial++;
+        }
+    }
+}*/
+
+var primeira Foto = document.get
+
+function mudarImagem ()
